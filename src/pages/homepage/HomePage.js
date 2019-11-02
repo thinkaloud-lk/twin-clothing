04f7f6cb076e2e3512120e50ee648916d9dbd792
Chinthaka Weerakkody <thinkaloud.lk@gmail.com>
2019-11-02 20:45:00 +0530

    initial commit

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/Container'
+import './homepage.scss'
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>  
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage;
